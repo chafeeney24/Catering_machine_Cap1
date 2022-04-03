e043ba9958c287754d807e6ae7239aecefffca4e
Colin McGregor <colin.w.mcgregor@gmail.com>
2022-04-03 12:13:31 -0500

    some more unecessary userInput tests

--- capstone/src/test/java/com/techelevator/view/UserInputTest.java
+++ capstone/src/test/java/com/techelevator/view/UserInputTest.java
@@ -11,88 +11,119 @@ import static org.junit.contrib.java.lang.system.TextFromStandardInputStream.emp
 public class UserInputTest {
 @Rule
     public final TextFromStandardInputStream systemInMock=emptyStandardInputStream();
+
+//ShowMainMenuTests
 @Test
-    public void lowerD(){
+    public void showMainMenuLowerD(){
     systemInMock.provideLines("d");
     String expected="Display";
     String actual=UserInput.showMainMenu();
     Assert.assertEquals(expected,actual);
 }
     @Test
-    public void capitalD(){
+    public void showMainMenuCapitalD(){
         systemInMock.provideLines("D");
         String expected="Display";
         String actual=UserInput.showMainMenu();
         Assert.assertEquals(expected,actual);
     }
     @Test
-    public void capitalP(){
+    public void showMainMenuCapitalP(){
         systemInMock.provideLines("P");
         String expected="Purchase";
         String actual=UserInput.showMainMenu();
         Assert.assertEquals(expected,actual);
     }
     @Test
-    public void lowerP(){
+    public void showMainMenuLowerP(){
         systemInMock.provideLines("p");
         String expected="Purchase";
         String actual=UserInput.showMainMenu();
         Assert.assertEquals(expected,actual);
     }
     @Test
-    public void lowerE(){
+    public void showMainMenuLowerE(){
         systemInMock.provideLines("e");
         String expected="Exit";
         String actual=UserInput.showMainMenu();
         Assert.assertEquals(expected,actual);
     }
     @Test
-    public void capitalE(){
+    public void showMainMenuCapitalE(){
         systemInMock.provideLines("E");
         String expected="Exit";
         String actual=UserInput.showMainMenu();
         Assert.assertEquals(expected,actual);
     }
     @Test
-    public void lowerS(){
+    public void showMainMenuLowerS(){
         systemInMock.provideLines("s");
         String expected="Sales Report";
         String actual=UserInput.showMainMenu();
         Assert.assertEquals(expected,actual);
     }
     @Test
-    public void capitalS(){
+    public void showMainMenuCapitalS(){
         systemInMock.provideLines("S");
         String expected="Sales Report";
         String actual=UserInput.showMainMenu();
         Assert.assertEquals(expected,actual);
     }
     @Test
-    public void lowerq(){
+    public void showMainMenuLowerq(){
         systemInMock.provideLines("q");
         String expected="Invalid";
         String actual=UserInput.showMainMenu();
         Assert.assertEquals(expected,actual);
     }
     @Test
-    public void lowerX(){
+    public void showMainMenuLowerX(){
         systemInMock.provideLines("x");
         String expected="Invalid";
         String actual=UserInput.showMainMenu();
         Assert.assertEquals(expected,actual);
     }
     @Test
-    public void lowerDP(){
+    public void showMainMenuLowerDP(){
         systemInMock.provideLines("dp");
         String expected="Invalid";
         String actual=UserInput.showMainMenu();
         Assert.assertEquals(expected,actual);
     }
     @Test
-    public void capitalED(){
+    public void showMainMenuCapitalED(){
         systemInMock.provideLines("ED");
         String expected="Invalid";
         String actual=UserInput.showMainMenu();
+        Assert.assertEquals(expected,actual);
+    }
+    //BottomOfDisplay Tests
+    @Test
+    public void bottomOfDisplayLowerP(){
+        systemInMock.provideLines("p");
+        String expected="Purchase";
+        String actual=UserInput.bottomOfDisplay();
+        Assert.assertEquals(expected,actual);
+    }
+    @Test
+    public void bottomOfDisplayCapitalP(){
+        systemInMock.provideLines("P");
+        String expected="Purchase";
+        String actual=UserInput.bottomOfDisplay();
+        Assert.assertEquals(expected,actual);
+    }
+    @Test
+    public void bottomOfDisplayLowerB(){
+        systemInMock.provideLines("b");
+        String expected="Back";
+        String actual=UserInput.bottomOfDisplay();
+        Assert.assertEquals(expected,actual);
+    }
+    @Test
+    public void bottomOfDisplayCapitalB(){
+        systemInMock.provideLines("B");
+        String expected="Back";
+        String actual=UserInput.bottomOfDisplay();
         Assert.assertEquals(expected,actual);
     }
 }
